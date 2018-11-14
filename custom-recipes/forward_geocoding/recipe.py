64b320cda5acf5c5f8f6aac84216b391a948c063
Dimitri Labouesse <dlabouesse@users.noreply.github.com>
2018-11-14 15:32:27 +0100

    Update recipe.py

--- custom-recipes/forward_geocoding/recipe.py
+++ custom-recipes/forward_geocoding/recipe.py
@@ -16,6 +16,7 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 def get_config():
     print('On est dans la nouvelle branche')
     print('le 3e')
+    print('le 3e bis')
     print('new line')
     config = {}
     config['input_ds'] = dataiku.Dataset(get_input_names_for_role('input_ds')[0])
